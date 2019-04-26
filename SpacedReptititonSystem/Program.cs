ae8f2a316edf5010546376f64bc301085ba9f623
LAPTOP-S30LJ72T\Charles <charlies_house2000@yahoo.com>
2019-04-26 23:04:44 +1200

    Add project files.

---
+++ SpacedReptititonSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpacedReptititonSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
